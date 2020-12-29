9523f57392a0cd1b4dc88379e8a9ddb7361cbc95
zed <zed@kolk.me>
2020-12-29 21:12:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,5 @@ end
 
 def find_valid_calls planeteer_calls
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |value|
-    value == valid_calls
-  end
+  planeteer_calls&valid_calls
 end
